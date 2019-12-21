382120e50adf403742b4fccbde6308bf6766c14d
Nick <kulakov.nick.n@gmail.com>
2019-12-21 16:16:15 +0200

    start

---
+++ UICollectionView/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UICollectionView
+//
+//  Created by Nick on 17.12.2019.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+//        window = UIWindow()
+        return true
+    }
+
+}
+
